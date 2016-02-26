554ea6d0cc83a3f2b4306515a5f9533cd77f7120
Andrew Phillips <theasp@gmail.com>
2016-02-26 14:52:36 -0400

    Change tag...

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ set -ex
 ORIGIN_URL=`git config --get remote.origin.url`
 GOOGLETAG="
 <!-- Google Tag Manager -->
-<noscript><iframe src=\"//www.googletagmanager.com/ns.html?id=GTM-PN298J\"
+<noscript><iframe src=\"//www.googletagmanager.com/ns.html?id=GTM-W84GVX\"
 height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PN298J');</script>
+})(window,document,'script','dataLayer','GTM-W84GVX');</script>
 <!-- End Google Tag Manager -->"
 
 # Set identity
