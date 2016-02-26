6ebd4f03e13ec957d32ef1105589085f2807d512
Andrew Phillips <theasp@gmail.com>
2016-02-26 14:47:39 -0400

    Add tag to index.html

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,16 @@ set -ex
 
 # Variables
 ORIGIN_URL=`git config --get remote.origin.url`
+GOOGLETAG="
+<!-- Google Tag Manager -->
+<noscript><iframe src=\"//www.googletagmanager.com/ns.html?id=GTM-PN298J\"
+height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PN298J');</script>
+<!-- End Google Tag Manager -->"
 
 # Set identity
 git config user.name "Automated Deployment"
@@ -29,6 +39,9 @@ mv .build/target/cljsbuild/public/* .
 # Remove the old files
 git rm --cached -r .
 rm -rf .build
+
+# Add google tag
+perl -p -i -e "s%<!-- @@GOOGLETAG@@ -->%$GOOGLETAG%" index.html
 
 # Push to gh-pages.
 git add -fA
