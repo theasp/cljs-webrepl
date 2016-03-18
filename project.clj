77be8326d358a29435ccec914fddf2cb4e5bf4bf
Andrew Phillips <theasp@gmail.com>
2016-03-18 19:34:34 -0300

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/clojurescript "1.8.34"]
                  [cljsjs/material "1.1.1-0"]
-                 [com.taoensso/timbre "4.2.1"]
+                 [com.taoensso/timbre "4.3.1"]
                  [cljsjs/clipboard "1.5.5-0"]
                  [environ "1.0.2"]
                  [figwheel "0.5.0-6"]
